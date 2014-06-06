14e961da8f8f1d8c7eacd9b7ccedad119edc5d79
psypersky <alkazar.r@gmail.com>
2014-06-06 11:17:49 -0500

    fix chat example

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ io.on('connection', function(socket){
   });
 });
 
-self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-if (typeof self.ipaddress === "undefined") {
+if (typeof ipaddress === "undefined") {
     console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-    self.ipaddress = "127.0.0.1";
+    ipaddress = "127.0.0.1";
 };
 
-http.listen(self.port, function(){
-  console.log('listening on *:', self.port);
+http.listen(port, function(){
+  console.log('listening on *:', port);
 });
+
 var express = require('express');
 var fs      = require('fs');
